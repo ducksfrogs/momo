bf1539fe3a96d5bd81aafa1e2aa5f290bfa651cd
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-13 17:51:53 +0900

    dd

--- app/scripts/main7.js
+++ app/scripts/main7.js
@@ -3,9 +3,25 @@ const canvas = d3.select('.canva');
 
 const svg = canvas.append('svg')
                 .attr('width', 600)
-                .attr('height', 400);
+                .attr('height', 500);
 
-const rect = svg.selectAll('rect');
+const margin = {top: 20, right: 20, bottom: 70, left:70}
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 500 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+                .attr("width", graphWidth)
+                .attr("height", graphHeight)
+                .attr("transform", `translate(${margin.left},
+                                              ${margin.top})`);
+
+const rect = graph.selectAll('rect');
+
+const xAxisGroup = graph.append("g")
+                  .attr("transform", `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append("g");
+
+
 
 d3.json('./scripts/test.json')
       .then(data => {
@@ -24,6 +40,9 @@ d3.json('./scripts/test.json')
                 .paddingInner(0.1)
                 .paddingOuter(0.2);
 
+
+
+
         console.log(data.map(item => item.fill ))
 
         // console.log(x("red"))
@@ -39,4 +58,10 @@ d3.json('./scripts/test.json')
               .attr('height', (d,i) => y(d.height))
               .attr('fill', (d, i) => d.fill)
               .attr('x', (d,i) => x(d.fill))
+
+              const xAxis = d3.axisBottom(x);
+              const yAxis = d3.axisLeft(y);
+
+              xAxisGroup.call(xAxis);
+              yAxisGroup.call(yAxis);
       })
