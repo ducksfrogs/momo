5c66fb8ffbe83bd1989b611b85dc41cc57a2f9af
ma <ma_yamaki@yahoo.com>
2021-02-13 14:44:32 +0900

    dd

--- app/scripts/main3.js
+++ app/scripts/main3.js
@@ -4,7 +4,8 @@ var width = 600;
 var height = 600;
 
 // const api_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson'
-const api_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson'
+// const api_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson'
+const api_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson'
 const canvas = d3.select('.canva');
 
 const svg = canvas.append('svg')
@@ -20,13 +21,13 @@ d3.json(api_url)
                       .data(data.features);
 
           circle.attr('cx', (d,i) => d.properties.mag)
-                .attr('cy', 14)
+                .attr('cy', (d, i) => Math.floor(Math.random()*100)+ d.properties.mag)
                 .attr('r', (d,i) => d.properties.mag)
-                .attr('fill', 'purple')
+                .attr('fill', (d, i) => d.properties.alert)
 //
           circle.enter().append('circle')
                 .attr('cx', (d,i) => d.properties.mag * 80)
-                .attr('cy', 14)
-                .attr('r', (d,i) => d.properties.mag*2)
-                .attr('fill', 'purple')
+                .attr('cy', (d, i) => Math.floor(Math.random()*100)+ d.properties.mag)
+                .attr('r', (d,i) => d.properties.mag*4)
+                .attr('fill', (d,i) => d.properties.alert)
         });
