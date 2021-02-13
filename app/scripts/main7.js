5aa3d6f245f9da433634779516f7a51eac355f2a
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-13 18:03:20 +0900

    dd

--- app/scripts/main7.js
+++ app/scripts/main7.js
@@ -25,14 +25,9 @@ const yAxisGroup = graph.append("g");
 
 d3.json('./scripts/test.json')
       .then(data => {
-          console.log(data)
           const y = d3.scaleLinear()
               .domain([0, d3.max(data, d => d.height)])
-              .range([0, 400]);
-
-          console.log(y);
-          console.log(y(250));
-          console.log(y(500));
+              .range([graphHeight, 0]);
 
       const x = d3.scaleBand()
                 .domain(data.map(item => item.fill ))
@@ -40,24 +35,17 @@ d3.json('./scripts/test.json')
                 .paddingInner(0.1)
                 .paddingOuter(0.2);
 
-
-
-
         console.log(data.map(item => item.fill ))
 
-        // console.log(x("red"))
-        // console.log(x("blue"))
-        // console.log(x("pink"))
-        // console.log(x("red"))
-        // console.log(x("red"))
 
         console.log(x.bandwidth());
           rect.data(data)
               .enter().append('rect')
               .attr("width", x.bandwidth)
-              .attr('height', (d,i) => y(d.height))
+              .attr('height', (d,i) => graphHeight - y(d.height))
               .attr('fill', (d, i) => d.fill)
               .attr('x', (d,i) => x(d.fill))
+              .attr('y', d => y(d.height))
 
               const xAxis = d3.axisBottom(x);
               const yAxis = d3.axisLeft(y);
