5ab80b7f6db98eb3dd43898b10c114565c287fad
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-15 10:10:10 +0900

    dd

--- app/scripts/vue.js
+++ app/scripts/vue.js
@@ -4,6 +4,19 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './assets/vmSocks-green.png'
+    image: './assets/vmSocks-green.png',
+    inStock:true,
+    inventory: 1,
+    details: ['80cottn', 'ddd', 'ddddd'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ]
   }
 })
